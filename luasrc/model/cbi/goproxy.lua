84fc8289796207ff86f045275e4287bd1ff4f2a8
sjz123321 <shijiezuan@gmail.com>
2025-06-24 21:42:51 +0800

    start process after 3 sec delayed
    Signed-off-by: sjz123321 <shijiezuan@gmail.com>

--- luasrc/model/cbi/goproxy.lua
+++ luasrc/model/cbi/goproxy.lua
@@ -1,47 +1,43 @@
 local sys = require "luci.sys"
 local http = require "luci.http"
 
--- 保存后自动重载服务（兼容所有 LuCI/OpenWrt 版本）
-if http.formvalue("cbi.apply") then
-    sys.call("/etc/init.d/goproxy restart >/dev/null 2>&1")
-end
-
 local function get_goproxy_status()
-    local status = luci.sys.exec("/etc/goproxy/check.sh 2>/dev/null"):gsub("\n", "")
+    local status = sys.exec("/etc/goproxy/check.sh 2>/dev/null"):gsub("\n", "")
     if status == "running" then
-        return "<span style='color:green;font-weight:bold'>正在运行</span>"
+        return "<span style='color:green;font-weight:bold'>............</span>"
     else
-        return "<span style='color:red;font-weight:bold'>未运行</span>"
+        return "<span style='color:red;font-weight:bold'>.........</span>"
     end
 end
 
-m = Map("goproxy", translate("Goproxy 设置"))
+m = Map("goproxy", translate("Goproxy ......"))
 m.apply_on_parse = true
+-- on_after_commit .............................................
 function m.on_after_commit(self)
-    luci.sys.call("/etc/init.d/goproxy restart >/dev/null 2>&1")
+    sys.call("sleep 1; /etc/init.d/goproxy restart >/dev/null 2>&1 &")
 end
 
-s = m:section(TypedSection, "main", translate("基础设置"))
+s = m:section(TypedSection, "main", translate("............"))
 s.anonymous = true
 
--- 状态
-local st = s:option(DummyValue, "_status", translate("Goproxy 运行状态"))
+-- ............
+local st = s:option(DummyValue, "_status", translate("Goproxy ............"))
 st.rawhtml = true
 st.default = get_goproxy_status()
 
--- 启用
-local o = s:option(Flag, "enable", translate("启用Goproxy"))
+-- ............
+local o = s:option(Flag, "enable", translate("......Goproxy"))
 o.rmempty = false
 
--- 参数模式
-o = s:option(ListValue, "param_mode", translate("参数模式"))
-o:value("default", translate("默认"))
-o:value("custom", translate("自定义命令行参数"))
+-- ............
+o = s:option(ListValue, "param_mode", translate("............"))
+o:value("default", translate("......"))
+o:value("custom", translate("........................"))
 
--- 自定义参数
-local custom_arg = s:option(Value, "custom_args", translate("自定义命令行参数"))
+-- ...............
+local custom_arg = s:option(Value, "custom_args", translate("........................"))
 custom_arg:depends("param_mode", "custom")
 custom_arg.placeholder = "--listen :8080 --target http://example.com"
-custom_arg.description = translate("无需添加 <b>--daemon --forever</b>，系统会自动附加这两个参数。")
+custom_arg.description = translate("............ <b>--daemon --forever</b>..........................................")
 
 return m
